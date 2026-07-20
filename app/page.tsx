0f22ed92f4548e0c099abdd267b7e3575bbb4f3c
Storka <storkayoussef@gmail.com>
2026-07-20 12:17:05 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <div className="space-y-6">
           <NetworkGuard />
           <BalanceLedger />
-          <DepositPanel />
+    
           <TransferPanel />
         </div>
 
